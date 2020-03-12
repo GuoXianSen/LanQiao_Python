6996f851a0b23180520fbb2375be52e74cdb9797
jingsongliu <45508593+jingsongliujing@users.noreply.github.com>
2020-03-12 18:05:31 +0800

    Create 基础练习 报时助手.py

---
+++ 基础练习/基础练习 报时助手.py
@@ -0,0 +1,31 @@
+a,b=map(int,input().split(" "))
+list=[
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+    'twenty',
+    ]
+
+for i in range(0,(a+1)):
+    continue
+    
+for j in range(0,(b+1)):
+    continue
+print(list[i],list[j])
